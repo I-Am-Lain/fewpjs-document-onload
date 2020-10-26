594a940c6f00340d304d2b8c8ec00f32f6f583df
I-Am-Lain <vetter.william@gmail.com>
2020-10-26 13:57:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+let x = document.querySelector("p#text")
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("The DOM has loaded");
+    x.textContent = 'This is really cool!'
+  });
